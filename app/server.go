183f8cb40c86f4a10376df7c76323cd3edf1e20d
Hemant D <h3mantd@Hemants-MacBook-Pro.local>
2024-07-28 19:48:07 +0530

    Responding with 200 Ok

--- app/server.go
+++ app/server.go
@@ -20,10 +20,14 @@ func main() {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
+	defer l.Close()
 
-	_, err = l.Accept()
+	req, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+
+	req.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+
 }
